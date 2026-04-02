6089cb3258513447d4e3a23f9bc221d2751fa9d8
Jonathan Tri <jonathantric@gmail.com>
2026-04-02 10:42:07 +0700

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { RootProvider } from "fumadocs-ui/provider/next";
 import type { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 const siteUrl = "https://rnjet.jonathantri.com";
 
@@ -113,6 +114,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 				>
 					{children}
 				</RootProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
